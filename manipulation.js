acd97ab2dfe7e1d789e58983dc7bb5c584e71196
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2024-11-11 13:37:53 +0100

    Added files and some initial code

---
+++ manipulation.js
@@ -0,0 +1,28 @@
+window.addEventListener("DOMContentLoaded", main);
+
+
+function main() {
+
+}
+
+function startGame (){
+
+
+}
+
+
+function showScene () {
+
+
+}
+
+
+function typeText () {
+
+
+}
+
+function changeBackground () {
+
+    
+}
