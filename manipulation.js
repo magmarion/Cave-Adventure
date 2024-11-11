2fc37f8d2c149bf07897995f161824298e260f4d
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2024-11-11 16:01:05 +0100

    add more functions and content

--- manipulation.js
+++ manipulation.js
@@ -29,10 +29,35 @@ function showScene() {
     switch (scene) {
         case "story":
             sceneText = `A young man wakes up in a dark cave, alone and disoriented. The only thing he remembers is a mysterious map hidden in his pocket, showing the way to a legendary treasure, the lost ark.`;
+            typeText(sceneText, () => {
+                setTimeout(() => showWhisperOptions(), 500);
+            });
             break;
 
+
         case "explore":
-            sceneText = `Following the whisper, he discovers an old sage who gives him a clue to the treasure. The Old Sage: "The wilderness is unforgiving. Learn to read the signs, and you'll find your way"`;
+            sceneText = `Following the whisper, he discovers an old sage who gives him a clue to the treasure. The Old Sage: "The wilderness is unforgiving. Learn to read the signs, and you'll find your way.`;
+             typeText(sceneText, () => {
+                content.innerHTML += `
+                    <div class="button-container">
+                        <button onclick="listenToTheOldMan()">Listen</button>
+                        <button onclick="showScene('story)">Continue</button>
+                    </div>
+                `;
+            });
+            break;
+
+            
+        case "ignore":
+            sceneText = `The character ignores the whisper and on his way he finds a mysterious message on a wall, giving him a clue about the journey ahead.`;
+            typeText(sceneText, () => {
+                content.innerHTML += `
+                    <div class="button-container">
+                        <button onclick="exploreText()">Read The Scripture</button>
+                        <button onclick="showScene('story')">Continue</button>
+                    </div>
+                `;
+            });
             break;
 
     }
@@ -61,7 +86,53 @@ function typeText(text, callback) {
 }
 
 
-function changeBackground() {
-
+function showWhisperOptions () {
 
 }
+
+function selectPath () {
+
+}
+
+
+
+function changeBackground() {
+    const body = document.body;
+
+    /* Define the background images for each scene */
+
+    switch (scene) {
+        case "story":
+            body.style.backgroundImage = "url('media/youngMan.webp')";
+            break;
+        case "story":
+                body.style.backgroundImage = "url('media/oldSage.png')";
+                break; 
+        case "story":
+                body.style.backgroundImage = "url('media/examine.webp')";
+                break;
+        default:
+            body.style.backgroundImage = "url('images_audio/cave.webp')";
+            break;
+
+    }
+
+    /* Add smooth transition on background change */
+    body.style.transition = "background-image 1s ease-in-out";
+
+}
+
+function showPopupMessage() {
+
+}
+
+
+function listenToTheOldMan () {
+    
+}
+
+
+function exploreText () {
+    
+}
+
