e5c0aee5ab826a7f6376fe07841ccbec580016db
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2024-11-11 15:33:58 +0100

    Add new functions in js and add containers for the content and options in html

--- manipulation.js
+++ manipulation.js
@@ -5,26 +5,63 @@ function main() {
 
 }
 
-function startGame (){
+function startGame() {
 
     document.getElementById("title").style.display = "none";
     document.getElementById("startScreen").style.display = "none";
 
-}
-
-
-function showScene () {
-
+    /* Show the game interface */
+    document.getElementById("sceneContainer").style.display = "flex";
 
 }
 
 
-function typeText () {
+function showScene() {
+    const content = document.getElementById("content");
+    const options = document.getElementById("options");
 
+    content.innerHTML = ""; /* Clear previous content */
+    options.innerHTML = ""; /* Clear previous options */
 
+    /* Define the text  based on the scene */
+    let sceneText = "";
+
+    switch (scene) {
+        case "story":
+            sceneText = `A young man wakes up in a dark cave, alone and disoriented. The only thing he remembers is a mysterious map hidden in his pocket, showing the way to a legendary treasure, the lost ark.`;
+            break;
+
+        case "explore":
+            sceneText = `Following the whisper, he discovers an old sage who gives him a clue to the treasure. The Old Sage: "The wilderness is unforgiving. Learn to read the signs, and you'll find your way"`;
+            break;
+
+    }
 }
 
-function changeBackground () {
 
-    
+function typeText(text, callback) {
+    const storyTextElement = document.getElementById("content");
+    storyTextElement.textContent = "";
+    storyTextElement.style.display = "block"; /* Show the story text */
+
+    let letterIndex = 0;
+    const typingSpeed = 5;
+
+    function type() {
+        if (letterIndex < text.length) {
+            storyTextElement.textContent += text[letterIndex];
+            letterIndex++;
+            setTimeout(type, typingSpeed);
+        } else {
+            if (callback) callback();
+        }
+    }
+
+    type();
+}
+
+
+function changeBackground() {
+
+
 }
